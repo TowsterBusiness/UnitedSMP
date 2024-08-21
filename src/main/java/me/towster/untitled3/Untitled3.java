197ee3faf3505876a47f0748d96d39d971431867
Towster_ <79161644+TowsterBusiness@users.noreply.github.com>
2024-08-20 21:30:32 -0700

    first

---
+++ src/main/java/me/towster/untitled3/Untitled3.java
@@ -0,0 +1,17 @@
+package me.towster.untitled3;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Untitled3 extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
