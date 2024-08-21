d7f8ff09da51449aaab05ee23795e289b75b5e02
Towster_ <79161644+TowsterBusiness@users.noreply.github.com>
2024-08-20 21:35:04 -0700

    wing functions

--- src/main/java/me/towster/untitled3/Untitled3.java
+++ src/main/java/me/towster/untitled3/Untitled3.java
@@ -15,7 +15,22 @@ public class Untitled3 extends JavaPlugin {
         instance = this;
         // Plugin startup logic
         this.getCommand("united-give").setExecutor(new GiveCommands());
+        Bukkit.getServer().getScheduler().scheduleSyncRepeatingTask(this, new Runnable(){
+            public void run() {
+                for (Player p : Bukkit.getOnlinePlayers()) {
+                    ItemStack is = p.getInventory().getChestplate();
+                    if (is == null) continue;
+                    if (!is.hasItemMeta()) continue;
+                    if (!is.getItemMeta().hasCustomModelData()) continue;
+                    if (is.getItemMeta().getCustomModelData() == 3000) {
+                        if (is.getDurability() > 402) {
+                            is.setDurability((short) (is.getDurability() - 1));
+                        }
+                    }
+                }
+            }
 
+        }, 0L, 1200L);
     }
 
     @Override
